704c21b0b6e054e90defe44bcec73804ed703e74
Vladislav Kondratuk <vladislav.kondratuk@rambler.ru>
2020-03-10 21:03:35 +0300

    spring literal values injection

--- 01-sping-core-demo/src/main/java/com/demo/spring/core/SetterDIApp.java
+++ 01-sping-core-demo/src/main/java/com/demo/spring/core/SetterDIApp.java
@@ -1,6 +1,6 @@
 package com.demo.spring.core;
 
-import com.demo.spring.core.core_logic.Coach;
+import com.demo.spring.core.core_logic.CricketCoach;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -17,10 +17,12 @@ public class SetterDIApp {
         ConfigurableApplicationContext context =
                 new ClassPathXmlApplicationContext(CONFIG_LOCATION);
 
-        Coach coach = context.getBean("cricketCoach", Coach.class);
+        CricketCoach coach = context.getBean("cricketCoach", CricketCoach.class);
 
         log.info("DI with setter -> daily workout: {}", coach.getDailyWorkout());
         log.info("DI with setter -> daily fortune: {}", coach.getDailyFortune());
+        log.info("Literal Values Injection -> email address: {}, team: {}",
+                coach.getEmailAddress(), coach.getTeam());
 
         context.close();
     }
--- 01-sping-core-demo/src/main/java/com/demo/spring/core/core_logic/CricketCoach.java
+++ 01-sping-core-demo/src/main/java/com/demo/spring/core/core_logic/CricketCoach.java
@@ -1,22 +1,34 @@
 package com.demo.spring.core.core_logic;
 
 import com.demo.spring.core.service.FortuneService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class CricketCoach implements Coach {
 
-    private static Logger log = LoggerFactory.getLogger(CricketCoach.class);
-
     private FortuneService fortuneService;
 
-    public CricketCoach() {
-        log.info("CricketCoach: inside no-arg constructor");
-    }
+    private String emailAddress;
+    private String team;
+
+    public CricketCoach() {}
 
     public void setFortuneService(FortuneService fortuneService) {
-        log.info("CricketCoach: inside setter method setFortuneService");
         this.fortuneService = fortuneService;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getTeam() {
+        return team;
+    }
+
+    public void setTeam(String team) {
+        this.team = team;
     }
 
     @Override
