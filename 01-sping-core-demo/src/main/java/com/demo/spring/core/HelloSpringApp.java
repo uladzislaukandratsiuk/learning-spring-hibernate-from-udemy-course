ba2829fdf54ac05bd3f476e6e4059e1615873ef1
Vladislav Kondratuk <vladislav.kondratuk@rambler.ru>
2020-03-10 19:59:39 +0300

    spring inversion of control - xml configuration

---
+++ 01-sping-core-demo/src/main/java/com/demo/spring/core/HelloSpringApp.java
@@ -0,0 +1,25 @@
+package com.demo.spring.core;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class HelloSpringApp {
+
+    public static Logger log = LoggerFactory.getLogger(HelloSpringApp.class);
+
+    public static final String CONFIG_LOCATION = "applicationContext.xml";
+
+    public static void main(String[] args) {
+
+        ConfigurableApplicationContext context =
+                new ClassPathXmlApplicationContext(CONFIG_LOCATION);
+
+        Coach coach = context.getBean("myCoach", Coach.class);
+
+        log.info("daily workout: {}", coach.getDailyWorkout());
+
+        context.close();
+    }
+}
