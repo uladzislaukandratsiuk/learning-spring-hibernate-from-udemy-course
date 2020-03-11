111afd414a2299438646012ccff9520f200eaefd
Vladislav Kondratuk <vladislav.kondratuk@rambler.ru>
2020-03-11 20:13:11 +0300

    spring configuration with java annotations - qualifiers for DI

--- 02-spring-annotations-demo/src/main/java/com/spring/annotations/demo/core/TennisCoach.java
+++ 02-spring-annotations-demo/src/main/java/com/spring/annotations/demo/core/TennisCoach.java
@@ -2,6 +2,7 @@ package com.spring.annotations.demo.core;
 
 import com.spring.annotations.demo.service.FortuneService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -10,7 +11,7 @@ public class TennisCoach implements Coach {
     private FortuneService fortuneService;
 
     @Autowired
-    public TennisCoach(FortuneService fortuneService) {
+    public TennisCoach(@Qualifier("randomFortuneService") FortuneService fortuneService) {
         this.fortuneService = fortuneService;
     }
 
