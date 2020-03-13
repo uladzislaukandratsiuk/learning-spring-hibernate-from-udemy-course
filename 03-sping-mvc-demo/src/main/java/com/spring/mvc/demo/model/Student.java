e9eb4142b8bce8d7d1d4aca6a82673e4713a6dc9
Vladislav Kondratuk <vladislav.kondratuk@rambler.ru>
2020-03-13 21:17:39 +0300

    spring mvc form tags - drop-down list

--- 03-sping-mvc-demo/src/main/java/com/spring/mvc/demo/model/Student.java
+++ 03-sping-mvc-demo/src/main/java/com/spring/mvc/demo/model/Student.java
@@ -1,11 +1,27 @@
 package com.spring.mvc.demo.model;
 
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class Student {
 
     private String firstName;
     private String lastName;
+    private String country;
+
+    private final Map<String, String> countryOptions;
 
     public Student() {
+        countryOptions = new LinkedHashMap<>();
+
+        countryOptions.put("BY", "Belarus");
+        countryOptions.put("RU", "Russia");
+        countryOptions.put("PL", "Poland");
+    }
+
+    public Map<String, String> getCountryOptions() {
+        return Collections.unmodifiableMap(countryOptions);
     }
 
     public String getFirstName() {
@@ -22,5 +38,22 @@ public class Student {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", country='" + country + '\'' +
+                '}';
     }
 }
--- 03-sping-mvc-demo/src/main/webapp/WEB-INF/view/student-confirmation.jsp
+++ 03-sping-mvc-demo/src/main/webapp/WEB-INF/view/student-confirmation.jsp
@@ -7,5 +7,9 @@
 </head>
 <body>
     The student is confirmed: ${student.firstName} ${student.lastName}
+
+    <br><br>
+
+    Country: ${student.country}
 </body>
 </html>
