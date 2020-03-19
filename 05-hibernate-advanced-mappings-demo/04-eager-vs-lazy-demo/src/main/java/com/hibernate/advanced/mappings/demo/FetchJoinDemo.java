cb36cbfd7264819881f75b345ba14226becd8e3c
Vladislav Kondratuk <vladislav.kondratuk@rambler.ru>
2020-03-19 21:27:30 +0300

    hibernate advanced mappings - HQL JOIN FETCH, get lazy data after session is closed

--- 05-hibernate-advanced-mappings-demo/04-eager-vs-lazy-demo/src/main/java/com/hibernate/advanced/mappings/demo/FetchJoinDemo.java
+++ 05-hibernate-advanced-mappings-demo/04-eager-vs-lazy-demo/src/main/java/com/hibernate/advanced/mappings/demo/FetchJoinDemo.java
@@ -6,12 +6,13 @@ import com.hibernate.advanced.mappings.demo.entity.InstructorDetail;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class EagerFetchDemo {
+public class FetchJoinDemo {
 
-    private static Logger log = LoggerFactory.getLogger(EagerFetchDemo.class);
+    private static Logger log = LoggerFactory.getLogger(FetchJoinDemo.class);
 
     public static void main(String[] args) {
 
@@ -31,14 +32,19 @@ public class EagerFetchDemo {
             session.beginTransaction();
 
             int id = 1;
-            Instructor instructor = session.get(Instructor.class, id);
+
+            Query<Instructor> query =
+                    session.createQuery("select i from Instructor i " +
+                                    "JOIN FETCH i.courses " +
+                                    "where i.id=:instructorId",
+                            Instructor.class);
+
+            query.setParameter("instructorId", id);
+
+            Instructor instructor = query.getSingleResult();
 
             if (instructor != null) {
-
                 log.info("Found instructor: {}", instructor);
-
-                log.info("Found courses: {}", instructor.getCourses());
-
             } else {
                 log.info("Instructor with id={}, is not found!", id);
             }
@@ -46,6 +52,16 @@ public class EagerFetchDemo {
             session.getTransaction().commit();
 
             log.info("Transaction committed!");
+
+            session.close();
+
+            log.info("Session closed!");
+
+            if (instructor != null) {
+                log.info("Found courses: {}", instructor.getCourses());
+            } else {
+                log.info("Instructor is not found! Can't get the courses!");
+            }
         }
     }
 }
