08ad4401a763ea6ef9d9ebe7b6f727641563131b
Vladislav Kondratuk <vladislav.kondratuk@rambler.ru>
2020-03-23 21:53:31 +0300

    database web app - add css for lis-customers, redirect page

--- 06-web-customer-tracker/src/main/java/com/web/customer/tracker/config/WebMvcConfigure.java
+++ 06-web-customer-tracker/src/main/java/com/web/customer/tracker/config/WebMvcConfigure.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.UrlBasedViewResolver;
@@ -23,5 +25,15 @@ public class WebMvcConfigure implements WebMvcConfigurer {
         viewResolver.setPrefix(RESOLVER_PREFIX);
         viewResolver.setSuffix(RESOLVER_SUFFIX);
         return viewResolver;
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("/index");
     }
 }
