850dfd821a13e8a1d2307001acd46da2d6688679
Vladislav Kondratuk <vladislav.kondratuk@rambler.ru>
2020-03-23 20:57:37 +0300

    database web app - add CustomerDAO getCustomers() implementation

---
+++ 06-web-customer-tracker/src/main/java/com/web/customer/tracker/dao/CustomerDAO.java
@@ -0,0 +1,10 @@
+package com.web.customer.tracker.dao;
+
+import com.web.customer.tracker.entity.Customer;
+
+import java.util.List;
+
+public interface CustomerDAO {
+
+   List<Customer> getCustomers();
+}
---
+++ 06-web-customer-tracker/src/main/java/com/web/customer/tracker/dao/CustomerDAOImpl.java
@@ -0,0 +1,30 @@
+package com.web.customer.tracker.dao;
+
+import com.web.customer.tracker.entity.Customer;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Repository
+public class CustomerDAOImpl implements CustomerDAO {
+
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    @Override
+    @Transactional
+    public List<Customer> getCustomers() {
+
+        Session session = sessionFactory.getCurrentSession();
+
+        Query<Customer> customersQuery =
+                session.createQuery("from Customer", Customer.class);
+
+        return customersQuery.getResultList();
+    }
+}
