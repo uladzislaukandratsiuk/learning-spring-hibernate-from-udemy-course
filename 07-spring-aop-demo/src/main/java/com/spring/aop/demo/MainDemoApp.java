9f59effcd069c1cd27725223a565f7c335fb8722
Vladislav Kondratuk <vladislav.kondratuk@rambler.ru>
2020-03-25 21:15:15 +0300

    spring aop - @Before advice - add and test AOP Aspect

--- 07-spring-aop-demo/src/main/java/com/spring/aop/demo/MainDemoApp.java
+++ 07-spring-aop-demo/src/main/java/com/spring/aop/demo/MainDemoApp.java
@@ -1,11 +1,16 @@
 package com.spring.aop.demo;
 
+import com.spring.aop.demo.aspect.MyDemoLoggingAspect;
 import com.spring.aop.demo.config.DemoConfig;
 import com.spring.aop.demo.dao.AccountDAO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MainDemoApp {
+
+    private static Logger log = LoggerFactory.getLogger(MainDemoApp.class);
 
     public static void main(String[] args) {
 
@@ -13,6 +18,10 @@ public class MainDemoApp {
                 new AnnotationConfigApplicationContext(DemoConfig.class);
 
         AccountDAO bean = context.getBean("accountDAO", AccountDAO.class);
+
+        bean.addAccount();
+
+        log.info("call addAccount() method again...");
 
         bean.addAccount();
 
---
+++ 07-spring-aop-demo/src/main/java/com/spring/aop/demo/aspect/MyDemoLoggingAspect.java
@@ -0,0 +1,19 @@
+package com.spring.aop.demo.aspect;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class MyDemoLoggingAspect {
+
+    private static Logger log = LoggerFactory.getLogger(MyDemoLoggingAspect.class);
+
+    @Before("execution(public void addAccount())")
+    public void beforeAddAccountAdvice() {
+        log.info("Executing @Before advice on addAccount()");
+    }
+}
