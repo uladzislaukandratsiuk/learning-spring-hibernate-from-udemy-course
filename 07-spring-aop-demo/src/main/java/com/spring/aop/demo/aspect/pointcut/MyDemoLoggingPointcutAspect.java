8f4db514b2941539f5deb7b7c7864878fed7121f
Vladislav Kondratuk <vladislav.kondratuk@rambler.ru>
2020-03-29 21:51:12 +0300

    spring aop - join points: access and display method signature

--- 07-spring-aop-demo/src/main/java/com/spring/aop/demo/aspect/pointcut/MyDemoLoggingPointcutAspect.java
+++ 07-spring-aop-demo/src/main/java/com/spring/aop/demo/aspect/pointcut/MyDemoLoggingPointcutAspect.java
@@ -1,8 +1,10 @@
 package com.spring.aop.demo.aspect.pointcut;
 
 import com.spring.aop.demo.aspect.MyDemoLoggingAspect;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.annotation.Order;
@@ -16,7 +18,10 @@ public class MyDemoLoggingPointcutAspect {
     private static Logger log = LoggerFactory.getLogger(MyDemoLoggingAspect.class);
 
     @Before("com.spring.aop.demo.aspect.pointcut.AopDeclaration.forCopyDaoPackageNoGetterSetter()")
-    public void beforeAnyMethodInPackageAdvice() {
+    public void beforeAnyMethodInPackageAdvice(JoinPoint joinPoint) {
         log.info("Executing @Before advice on any method in com.spring.aop.demo.copydao.* package");
+
+        MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
+        log.info("Method signature:" + methodSignature);
     }
 }
