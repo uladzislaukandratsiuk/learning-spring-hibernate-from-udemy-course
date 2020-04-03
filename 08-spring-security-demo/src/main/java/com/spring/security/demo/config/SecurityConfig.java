e375d9d0e03ae2890a3215c7a4d6aee7d6ba3648
Vladislav Kondratuk <vladislav.kondratuk@rambler.ru>
2020-04-03 22:34:29 +0300

    spring security - create Security Initializer, Security Config with basic security (users, passwords and roles)

---
+++ 08-spring-security-demo/src/main/java/com/spring/security/demo/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.spring.security.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        User.UserBuilder users = User.withDefaultPasswordEncoder();
+
+        auth.inMemoryAuthentication()
+                .withUser(users.username("john").password("test123").roles("EMPLOYEE"))
+                .withUser(users.username("marry").password("test123").roles("MANAGER"))
+                .withUser(users.username("andrew").password("test123").roles("ADMIN"));
+    }
+}
---
+++ 08-spring-security-demo/src/main/java/com/spring/security/demo/config/SecurityWebAppInitializer.java
@@ -0,0 +1,6 @@
+package com.spring.security.demo.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebAppInitializer extends AbstractSecurityWebApplicationInitializer {
+}
