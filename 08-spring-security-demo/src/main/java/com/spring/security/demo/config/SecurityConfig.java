1bb339b8064788d2dca90dae93e8f549f84462ae
Vladislav Kondratuk <vladislav.kondratuk@rambler.ru>
2020-04-04 22:08:00 +0300

    spring security - custom login form: configure HttpSecurity for login page, add LoginController and login .jsp form

--- 08-spring-security-demo/src/main/java/com/spring/security/demo/config/SecurityConfig.java
+++ 08-spring-security-demo/src/main/java/com/spring/security/demo/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.spring.security.demo.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -19,5 +20,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .withUser(users.username("john").password("test123").roles("EMPLOYEE"))
                 .withUser(users.username("marry").password("test123").roles("MANAGER"))
                 .withUser(users.username("andrew").password("test123").roles("ADMIN"));
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login-page")
+                .loginProcessingUrl("/authenticate-the-user")
+                .permitAll();
     }
 }
