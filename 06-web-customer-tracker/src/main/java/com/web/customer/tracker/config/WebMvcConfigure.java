5720463f841eb77b7a57c141b6db8b753ef91565
Vladislav Kondratuk <vladislav.kondratuk@rambler.ru>
2020-04-12 23:26:36 +0300

    adding spring security to CRM app - refactoring DataSource and Mvc configuration into one file config

--- 06-web-customer-tracker/src/main/java/com/web/customer/tracker/config/WebMvcConfigure.java
+++ 06-web-customer-tracker/src/main/java/com/web/customer/tracker/config/WebMvcConfigure.java
@@ -1,9 +1,15 @@
 package com.web.customer.tracker.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+import org.hibernate.SessionFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.*;
+import org.springframework.core.env.Environment;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -12,14 +18,88 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.UrlBasedViewResolver;
 
+import javax.sql.DataSource;
+import java.beans.PropertyVetoException;
+import java.util.Objects;
+import java.util.Properties;
+
 @Configuration
 @EnableWebMvc
 @EnableAspectJAutoProxy
+@EnableTransactionManagement
 @ComponentScan(basePackages = "com.web.customer.tracker")
+@PropertySource("classpath:persistence-mysql.properties")
 public class WebMvcConfigure implements WebMvcConfigurer {
 
     public static final String RESOLVER_PREFIX = "/WEB-INF/view/";
     public static final String RESOLVER_SUFFIX = ".jsp";
+
+    private static final Logger log = LoggerFactory.getLogger(WebMvcConfigure.class);
+
+    @Autowired
+    private Environment env;
+
+    @Bean
+    public DataSource crmDataSource() {
+
+        ComboPooledDataSource crmDataSource = new ComboPooledDataSource();
+
+        try {
+            crmDataSource.setDriverClass(env.getProperty("jdbc.driver"));
+        } catch (PropertyVetoException exc) {
+            throw new RuntimeException(exc);
+        }
+
+        log.info("jdbc.url=" + env.getProperty("jdbc.url"));
+        log.info("jdbc.user=" + env.getProperty("jdbc.user"));
+
+        crmDataSource.setJdbcUrl(env.getProperty("jdbc.url"));
+        crmDataSource.setUser(env.getProperty("jdbc.user"));
+        crmDataSource.setPassword(env.getProperty("jdbc.password"));
+
+        crmDataSource.setInitialPoolSize(getIntProperty("connection.pool.initialPoolSize"));
+        crmDataSource.setMaxPoolSize(getIntProperty("connection.pool.maxPoolSize"));
+        crmDataSource.setMinPoolSize(getIntProperty("connection.pool.minPoolSize"));
+        crmDataSource.setMaxIdleTime(getIntProperty("connection.pool.maxIdleTime"));
+
+        return crmDataSource;
+    }
+
+    private int getIntProperty(String propName) {
+        return Integer.parseInt(Objects.requireNonNull(env.getProperty(propName)));
+    }
+
+    private Properties getHibernateProperties() {
+
+        Properties props = new Properties();
+
+        props.setProperty("hibernate.dialect", env.getProperty("hibernate.dialect"));
+        props.setProperty("hibernate.show_sql", env.getProperty("hibernate.show_sql"));
+
+        return props;
+    }
+
+    @Bean
+    public LocalSessionFactoryBean sessionFactory() {
+
+        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+
+        sessionFactory.setDataSource(crmDataSource());
+        sessionFactory.setPackagesToScan(env.getProperty("hibernate.packagesToScan"));
+        sessionFactory.setHibernateProperties(getHibernateProperties());
+
+        return sessionFactory;
+    }
+
+    @Bean
+    @Autowired
+    public HibernateTransactionManager transactionManager(SessionFactory sessionFactory) {
+
+        HibernateTransactionManager txManager = new HibernateTransactionManager();
+        txManager.setSessionFactory(sessionFactory);
+
+        return txManager;
+    }
 
     @Bean
     public ViewResolver viewResolver() {
