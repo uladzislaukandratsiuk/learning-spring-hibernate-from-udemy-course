fa9bfd52c1999e6a15bf6809a16fd30285713ffe
Vladislav Kondratuk <vladislav.kondratuk@rambler.ru>
2020-04-19 23:29:52 +0300

    json data binding - jackson processing ignore unknown property

--- 09-spring-rest-demo/01-json-data-binding/src/main/java/com/json/data/binding/Student.java
+++ 09-spring-rest-demo/01-json-data-binding/src/main/java/com/json/data/binding/Student.java
@@ -1,7 +1,10 @@
 package com.json.data.binding;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import java.util.List;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Student {
 
     private int id;
