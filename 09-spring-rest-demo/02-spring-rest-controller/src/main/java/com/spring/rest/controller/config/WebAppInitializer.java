06bd82657b1d2aceacf4a877940de074737f15f7
Vladislav Kondratuk <vladislav.kondratuk@rambler.ru>
2020-04-20 23:11:26 +0300

    spring rest controller - set up configuration

---
+++ 09-spring-rest-demo/02-spring-rest-controller/src/main/java/com/spring/rest/controller/config/WebAppInitializer.java
@@ -0,0 +1,29 @@
+package com.spring.rest.controller.config;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+public class WebAppInitializer implements WebApplicationInitializer {
+
+    private static final String DISPATCHER_SERVLET_NAME = "dispatcher";
+
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+
+        AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+        context.register(WebConfig.class);
+
+        DispatcherServlet dispatcherServlet = new DispatcherServlet(context);
+
+        ServletRegistration.Dynamic registration =
+                servletContext.addServlet(DISPATCHER_SERVLET_NAME, dispatcherServlet);
+
+        registration.setLoadOnStartup(1);
+        registration.addMapping("/");
+    }
+}
---
+++ 09-spring-rest-demo/02-spring-rest-controller/src/main/java/com/spring/rest/controller/config/WebConfig.java
@@ -0,0 +1,11 @@
+package com.spring.rest.controller.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.spring.rest.controller")
+public class WebConfig {
+}
