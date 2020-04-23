362911f7ccccbe50e203f507c779b2be5e28a40d
Vladislav Kondratuk <vladislav.kondratuk@rambler.ru>
2020-04-23 23:30:31 +0300

    spring rest controller - exception handling: update rest service to throw exception, add exception handler

--- 09-spring-rest-demo/02-spring-rest-controller/src/main/java/com/spring/rest/controller/restcontroller/StudentRestController.java
+++ 09-spring-rest-demo/02-spring-rest-controller/src/main/java/com/spring/rest/controller/restcontroller/StudentRestController.java
@@ -1,10 +1,11 @@
 package com.spring.rest.controller.restcontroller;
 
 import com.spring.rest.controller.entity.Student;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.spring.rest.controller.exception.StudentErrorResponse;
+import com.spring.rest.controller.exception.StudentNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.PostConstruct;
 import java.util.ArrayList;
@@ -31,6 +32,21 @@ public class StudentRestController {
 
     @GetMapping("/students/{studentId}")
     public Student getStudent(@PathVariable int studentId) {
+        if (studentId >= students.size() || studentId < 0) {
+            throw new StudentNotFoundException("Student with id=" + studentId + " not found!");
+        }
         return students.get(studentId);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException exc) {
+
+        StudentErrorResponse error = new StudentErrorResponse();
+
+        error.setStatus(HttpStatus.NOT_FOUND.value());
+        error.setMessage(exc.getMessage());
+        error.setTimeStamp(System.currentTimeMillis());
+
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
     }
 }
