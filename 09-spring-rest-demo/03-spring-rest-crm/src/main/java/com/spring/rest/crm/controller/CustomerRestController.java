56a3c2e976a6458ecef9be62834c54db935ef44c
Vladislav Kondratuk <vladislav.kondratuk@rambler.ru>
2020-04-24 22:55:59 +0300

    spring rest crm - CRUD database: adding mapping for get customer by id request

--- 09-spring-rest-demo/03-spring-rest-crm/src/main/java/com/spring/rest/crm/controller/CustomerRestController.java
+++ 09-spring-rest-demo/03-spring-rest-crm/src/main/java/com/spring/rest/crm/controller/CustomerRestController.java
@@ -4,6 +4,7 @@ import com.spring.rest.crm.entity.Customer;
 import com.spring.rest.crm.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,5 +20,10 @@ public class CustomerRestController {
     @GetMapping("/customers")
     public List<Customer> getCustomers() {
         return customerService.getCustomers();
+    }
+
+    @GetMapping("/customers/{customerId}")
+    public Customer getCustomer(@PathVariable int customerId) {
+        return customerService.getCustomer(customerId);
     }
 }
