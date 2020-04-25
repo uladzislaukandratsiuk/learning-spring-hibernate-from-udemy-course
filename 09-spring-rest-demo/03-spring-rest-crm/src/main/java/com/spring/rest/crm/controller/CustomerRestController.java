872b946a252ab09b7492e193fd8c33684e3aaea2
Vladislav Kondratuk <vladislav.kondratuk@rambler.ru>
2020-04-25 23:50:46 +0300

    spring rest crm - CRUD database: update customer feature

--- 09-spring-rest-demo/03-spring-rest-crm/src/main/java/com/spring/rest/crm/controller/CustomerRestController.java
+++ 09-spring-rest-demo/03-spring-rest-crm/src/main/java/com/spring/rest/crm/controller/CustomerRestController.java
@@ -39,4 +39,10 @@ public class CustomerRestController {
         customerService.saveCustomer(customer);
         return customer;
     }
+
+    @PutMapping("/customers")
+    public Customer updateCustomer(@RequestBody Customer customer) {
+        customerService.saveCustomer(customer);
+        return customer;
+    }
 }
