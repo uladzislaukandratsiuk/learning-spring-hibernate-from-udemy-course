a75e261eca0ef65787454a9ce675a169dde871a6
Vladislav Kondratuk <vladislav.kondratuk@rambler.ru>
2020-04-25 22:48:01 +0300

    spring rest crm - exception handling: adding CustomErrorResponse and CustomerNotFoundException classes

---
+++ 09-spring-rest-demo/03-spring-rest-crm/src/main/java/com/spring/rest/crm/error/CustomErrorResponse.java
@@ -0,0 +1,41 @@
+package com.spring.rest.crm.error;
+
+public class CustomErrorResponse {
+
+    private int status;
+    private String message;
+    private long timeStamp;
+
+    public CustomErrorResponse() {
+    }
+
+    public CustomErrorResponse(int status, String message, long timeStamp) {
+        this.status = status;
+        this.message = message;
+        this.timeStamp = timeStamp;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public long getTimeStamp() {
+        return timeStamp;
+    }
+
+    public void setTimeStamp(long timeStamp) {
+        this.timeStamp = timeStamp;
+    }
+}
