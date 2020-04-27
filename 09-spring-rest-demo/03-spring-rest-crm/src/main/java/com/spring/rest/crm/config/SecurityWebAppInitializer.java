ade10de07385e0558aa58dbba5ebe75a5b4a324f
Vladislav Kondratuk <vladislav.kondratuk@rambler.ru>
2020-04-27 22:34:47 +0300

    spring rest crm - spring security: basic in memory authentication

---
+++ 09-spring-rest-demo/03-spring-rest-crm/src/main/java/com/spring/rest/crm/config/SecurityWebAppInitializer.java
@@ -0,0 +1,6 @@
+package com.spring.rest.crm.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebAppInitializer extends AbstractSecurityWebApplicationInitializer {
+}
