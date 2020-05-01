7673557fa2e51608ea88337fade8c4c4cb206d62
Vladislav Kondratuk <vladislav.kondratuk@rambler.ru>
2020-05-01 22:58:27 +0300

    spring boot overview - using custom application properties

--- 10-sping-boot-demo/01-spring-boot-overview/src/main/java/com/springboot/overview/rest/SimpleRestController.java
+++ 10-sping-boot-demo/01-spring-boot-overview/src/main/java/com/springboot/overview/rest/SimpleRestController.java
@@ -1,5 +1,6 @@
 package com.springboot.overview.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +8,12 @@ import java.time.LocalDateTime;
 
 @RestController
 public class SimpleRestController {
+
+    @Value("${instructor.name}")
+    private String instructorName;
+
+    @Value("${course.name}")
+    private String courseName;
 
     @GetMapping("/")
     public String sayHello() {
@@ -16,5 +23,10 @@ public class SimpleRestController {
     @GetMapping("/dev-tools")
     public String devTools() {
         return "Automatic reloading with DevTools!";
+    }
+
+    @GetMapping("/course-info")
+    public String getCourseInfo() {
+        return "Instructor: " + instructorName + ", Course: " + courseName;
     }
 }
