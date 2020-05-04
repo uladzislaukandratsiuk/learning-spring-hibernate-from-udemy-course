e66ec271844ded38b07f64d87db5e4ef24e42a31
Vladislav Kondratuk <vladislav.kondratuk@rambler.ru>
2020-05-05 00:00:01 +0300

    spring boot: spring data jpa rest crud api - configuration, pagination and sorting

--- 10-sping-boot-demo/03-spring-data-jpa-rest-crud-api/src/main/java/com/rest/crud/api/dao/EmployeeRepository.java
+++ 10-sping-boot-demo/03-spring-data-jpa-rest-crud-api/src/main/java/com/rest/crud/api/dao/EmployeeRepository.java
@@ -2,6 +2,8 @@ package com.rest.crud.api.dao;
 
 import com.rest.crud.api.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 }
